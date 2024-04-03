7172f1dc02b656e4903355840fc620f95f1d4f16
muneebah qayyum <mqayyum1@live.maryville.edu>
2024-04-03 13:01:18 -0500

    Created endpoint and html page for admin login

--- src/main/java/com/Lab/Information/System/LIS/Controller/Admin.java
+++ src/main/java/com/Lab/Information/System/LIS/Controller/Admin.java
@@ -2,11 +2,14 @@ package com.Lab.Information.System.LIS.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class Admin {
 
-    @GetMapping("/admin")
+    @RequestMapping(value = "/admin", method = {RequestMethod.GET})
     public String adminIndex(){
         return "Admin-Index.html";
     }
